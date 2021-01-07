10ac8aa96d25633de1a8250ffdaee6db1b3356a3
kaushikpuka1998 <kaushikghosh199832@gmail.com>
2021-01-07 22:33:04 +0530

    Form Data Submitting into database

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,8 +13,27 @@ class PostsController < ApplicationController
 
     end
 
-    def new
+    def new 
+       @post =  Post.new
+    end
+
+    def create
+        post = Post.new(post_params)
+
+        if post.save
+            
+            redirect_to post_path(post)
+            
+        else
+            redirect_to new_post_path
+        end
 
     end
+
+    private
+    def post_params
+        params.require(:post).permit(:title,:text)
+    end
+        
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ Rails.application.routes.draw do
 
   
   get '/posts', to:'posts#index'
+  post '/posts', to:'posts#create'
   get '/posts/new', to: 'posts#new', as: 'new_post'
+  
   get '/posts/:id', to: 'posts#show', as: 'post'
+  
  
  
 
